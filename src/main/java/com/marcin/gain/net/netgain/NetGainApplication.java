bb058adef3001ea008605963838d44a2753f2e07
“marcinp55 <marcinp55@gmail.com>
2019-03-21 19:34:59 +0100

    Init commit

---
+++ src/main/java/com/marcin/gain/net/netgain/NetGainApplication.java
@@ -0,0 +1,13 @@
+package com.marcin.gain.net.netgain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetGainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetGainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcin/gain/net/netgain/NetGainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marcin.gain.net.netgain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetGainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
